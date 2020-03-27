ecdb39cb30e831269d96623a43a63737c788b7c1
Mick Veldhuis <mickveldhuis@hotmail.nl>
2020-03-27 11:56:57 +0100

    Disable Alembic for the time being

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 class Config:
     DEBUG = False
-    UPLOAD_FOLDER = 'marky/static/uploads/'
+    UPLOAD_FOLDER = 'mark_one/static/uploads/'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'please_change_later')
 
 class DevConfig(Config):
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(Path().resolve() / 'marky' / 'database' / 'testing.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/marky_dev.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/mark_one_dev.db'
 
 class ProdConfig(Config):
     pass
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/marky_testing.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/mark_one_testing.db'
 
 configs = {
     'dev': DevConfig,
